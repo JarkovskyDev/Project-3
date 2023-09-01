cc7148321a5759cbc5f33e2b840abcde7f60d5a8
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-09-01 22:38:10 +0200

    top part almost complete

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ function App() {
   //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=2fcd0dc7028772818ff82344ed26a1d7`
 
   return (
-    <div className="App">
-    
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="huminidy">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
